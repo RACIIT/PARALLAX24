0d2b20006d30b3cb302a97af53b2010455360ae3
RACIIT <144658105+RACIIT@users.noreply.github.com>
2024-01-11 18:37:27 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/parallax24/",
+  base: "https://parallax24.rciit.org/",
   plugins: [react()],
 });
